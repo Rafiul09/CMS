374db275f62b169cc9d055a75da8989b81d2af52
Rafiul09 <rafiulxlr8@gmail.com>
2024-08-31 02:01:58 +0600

    problem in fixed tag @command

--- client/app/(route)/search/_components/CategoryList.jsx
+++ client/app/(route)/search/_components/CategoryList.jsx
@@ -17,7 +17,7 @@ import Link from 'next/link'
 
 function CategoryList() {
     return (
-        <div className='h-screen  mt-5 flex flex-col  '>
+        <div className='h-screen fixed mt-5 flex flex-col  '>
             <Command>
                 <CommandInput placeholder="Type a command or search..." />
                 <CommandList className='overflow-visible'>
